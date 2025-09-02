ec7df41fde3766621d0754149f7add3fd2136b67
2uwls <2uwls1126@gmail.com>
2025-09-02 12:54:09 +0900

    feat: 헤더, 내브바, 기본 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/common/Header';
+import Navbar from './components/common/Navbar';
+import Home from './pages/Home';
+import Guide from './pages/Guide';
+import Support from './pages/Support';
+import Community from './pages/Community';
+import MyPage from './pages/MyPage';
+
 function App() {
   return (
-    <div className="text-3xl font-bold underline text-primary">
-      Hello World
+    <div
+      style={{
+        width: 375,
+        height: 812,
+        margin: '0 auto',
+        boxShadow: '0 0 24px 0 rgba(0,0,0,0.08)',
+        position: 'relative',
+        overflow: 'hidden',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <BrowserRouter>
+        <Header />
+        <main className="pb-20" style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/guide" element={<Guide />} />
+            <Route path="/support" element={<Support />} />
+            <Route path="/community" element={<Community />} />
+            <Route path="/mypage" element={<MyPage />} />
+          </Routes>
+        </main>
+        <Navbar />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,43 @@
+import { colors } from '../../styles/colors';
+import { Link, useLocation } from 'react-router-dom';
+import {
+  FaPen,
+  FaFolderOpen,
+  FaHome,
+  FaComments,
+  FaUser,
+} from 'react-icons/fa';
+
+const navItems = [
+  { to: '/guide', label: '대출가이드', icon: <FaPen size={24} /> },
+  { to: '/support', label: '공공지원금', icon: <FaFolderOpen size={24} /> },
+  { to: '/', label: '홈', icon: <FaHome size={24} /> },
+  { to: '/community', label: '커뮤니티', icon: <FaComments size={24} /> },
+  { to: '/mypage', label: '마이페이지', icon: <FaUser size={24} /> },
+];
+
+const Navbar = () => {
+  const location = useLocation();
+  return (
+    <nav
+      className="absolute bottom-0 w-full flex bg-grey-50 z-50"
+      style={{ borderTop: `1px solid ${colors.gray}` }}
+    >
+      {navItems.map((item) => (
+        <Link
+          key={item.to}
+          to={item.to}
+          className={`flex-1 py-3 flex flex-col items-center font-bold`}
+          style={{
+            color: location.pathname === item.to ? colors.navy : '#222',
+          }}
+        >
+          {item.icon}
+          <span>{item.label}</span>
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Navbar;
